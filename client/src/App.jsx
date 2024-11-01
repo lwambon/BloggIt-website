613ccf5a2af9892c9213f85332510c2936d46cdd
lwambon <lwambonyaga@gmail.com>
2024-11-01 15:39:54 +0300

    sign up page backend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,6 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "react-query";
 import Header from "./component/Header/Header";
 import Home from "./pages/Home/Home";
 import WriteBlog from "./pages/WriteBlog/WriteBlog";
@@ -9,20 +10,23 @@ import Profile from "./pages/MyProfile/Profile";
 import Login from "./component/Login/Login";
 import SignUp from "./component/SignUp/SignUp";
 
+const client = new QueryClient();
 function App() {
   return (
-    <BrowserRouter>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/write" element={<WriteBlog />} />
-        <Route path="/explore" element={<ExploreBlog />} />
-        <Route path="/blogs" element={<MyBlogs />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/login" element={<Login />} />
-      </Routes>
-    </BrowserRouter>
+    <QueryClientProvider client={client}>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/write" element={<WriteBlog />} />
+          <Route path="/explore" element={<ExploreBlog />} />
+          <Route path="/blogs" element={<MyBlogs />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
   );
 }
 
--- client/src/component/SignUp/SignUp.jsx
+++ client/src/component/SignUp/SignUp.jsx
@@ -1,12 +1,64 @@
 import "./SignUp.css";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { useMutation } from "react-query";
+import { useState } from "react";
 
 function SignUp() {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [userName, setUserName] = useState("");
+  const [emailAddress, setEmailAddress] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [formError, setFormError] = useState("");
+
+  const navigate = useNavigate();
+
+  const { mutate, isloading } = useMutation({
+    mutationFn: async (newUser) => {
+      const response = await fetch("http://localhost:4000/users", {
+        method: "POST",
+        body: JSON.stringify(newUser),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      if (!response.ok) {
+        const error = await response.json();
+        throw new Error(error.message);
+      }
+      const data = response.json();
+      return data;
+    },
+    onSuccess: () => {
+      navigate("/login");
+    },
+    onError: (error) => {
+      setFormError(error.message || "Something went wrong!");
+    },
+  });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (password !== confirmPassword) {
+      setFormError("Passwords do not match");
+      return;
+    }
+
+    mutate({
+      firstName,
+      lastName,
+      userName,
+      emailAddress,
+      password,
+    });
+  };
+
   return (
     <div className="loggin-section">
       <div className="login-container">
         <h2>Create your account here</h2>
-        <form action="" className="login-details">
+        <form onSubmit={handleSubmit} className="login-details">
           <div className="inputs-details">
             <label htmlFor="firstname">First Name</label>
             <input
@@ -14,22 +66,45 @@ function SignUp() {
               id="firstName"
               placeholder="First Name"
               required
+              value={firstName}
+              onChange={(e) => setFirstName(e.target.value)}
             />
           </div>
 
           <div className="inputs-details">
             <label htmlFor="lastname">Last Name</label>
-            <input type="text" id="lastName" placeholder="Last Name" required />
+            <input
+              type="text"
+              id="lastName"
+              placeholder="Last Name"
+              required
+              value={lastName}
+              onChange={(e) => setLastName(e.target.value)}
+            />
           </div>
 
           <div className="inputs-details">
             <label htmlFor="email">Email</label>
-            <input type="email" id="email" placeholder="Email" required />
+            <input
+              type="email"
+              id="email"
+              placeholder="Email"
+              required
+              value={emailAddress}
+              onChange={(e) => setEmailAddress(e.target.value)}
+            />
           </div>
 
           <div className="inputs-details">
             <label htmlFor="userName">Username</label>
-            <input type="text" id="userName" placeholder="Username" required />
+            <input
+              type="text"
+              id="userName"
+              placeholder="Username"
+              required
+              value={userName}
+              onChange={(e) => setUserName(e.target.value)}
+            />
           </div>
 
           <div className="inputs-details">
@@ -39,6 +114,8 @@ function SignUp() {
               id="password"
               placeholder="Password"
               required
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
             />
           </div>
 
@@ -49,11 +126,17 @@ function SignUp() {
               id="confirm-password"
               placeholder="Confirm Password"
               required
+              value={confirmPassword}
+              onChange={(e) => setConfirmPassword(e.target.value)}
             />
           </div>
 
+          {formError && <p className="error-text">{formError}</p>}
+
           <div className="login-submit">
-            <button>Sign Up</button>
+            <button type="submit" disabled={isloading}>
+              {isloading ? "Loading, please wait..." : "Sign Up"}
+            </button>
           </div>
           <p className="login-text">
             Already have an account? <Link to="/login">Login here</Link>
